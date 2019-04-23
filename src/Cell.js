d9b38b249f4e374b3e99b55800f830895071db55
Dani Lyn <dlyn1110@gmail.com>
2019-04-23 13:03:32 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+
+  updateColor = () => {
+    let selectedColor = this.props.getColor()
+    this.selectedColor({color: selectedColor})
+  }
+
   render() {
     return (
       <div className="cell" style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
-  
+
 }
